580d960438d6e23ca7a94c814680c4096b7193f8
sandaluthushan20 <sandaluthushan20@gmail.com>
2024-06-17 12:58:16 +0530

    class roomm

---
+++ page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Navbar from './Navbar';
+
+
+export default function Home() {
+  return (
+  <div className="div">
+
+
+ 
+
+      <h1>Hello World</h1>
+  
+  </div>
+
+  );
+}
+ 
